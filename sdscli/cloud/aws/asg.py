1e7ebceab34c989a77c40ea4fdb1a1fecd759e13
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2026-04-22 11:33:07 -0700

    remove imports as they're not needed

--- sdscli/cloud/aws/asg.py
+++ sdscli/cloud/aws/asg.py
@@ -9,7 +9,7 @@ from pprint import pformat
 from collections import OrderedDict
 from operator import itemgetter
 
-from prompt_toolkit.shortcuts import prompt, print_tokens
+from prompt_toolkit.shortcuts import prompt
 from prompt_toolkit.styles import style_from_dict
 from prompt_toolkit.validation import Validator, ValidationError
 from pygments.token import Token
--- sdscli/cloud/aws/storage.py
+++ sdscli/cloud/aws/storage.py
@@ -11,7 +11,7 @@ from collections import OrderedDict
 from operator import itemgetter
 from fabric.api import execute, hide
 
-from prompt_toolkit.shortcuts import prompt, print_tokens
+from prompt_toolkit.shortcuts import prompt
 from prompt_toolkit.styles import style_from_dict
 from prompt_toolkit.validation import Validator, ValidationError
 from pygments.token import Token
